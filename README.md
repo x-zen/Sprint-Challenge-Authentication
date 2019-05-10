8b172b58928541b2177d54a0ad22fb1130ab6395
x-zen <jdharris.x@gmail.com>
2019-05-10 12:51:29 -0500

    MVP

--- README.md
+++ README.md
@@ -29,9 +29,22 @@ Implement an User Authentication System in order to access the jokes from the Jo
 Demonstrate your understanding of this week's concepts by answering the following free-form questions. Edit this document to include your answers after each question. Make sure to leave a blank line above and below your answer so it is clear and easy to read by your project manager.
 
 1. What is the purpose of using _sessions_?
+
+Limit how long an authentication is valid
+
 1. What does bcrypt do to help us store passwords in a secure manner.
+
+password hashing function.
+implements salting both manual and automatically.
+accumulative hashing rounds.
+
 1. What does bcrypt do to slow down attackers?
+
+add time to our security algorithm to produce what is known as a Key Derivation Function
+
 1. What are the three parts of the JSON Web Token?
+
+header, payload, signature
 
 ## Project Set Up
 
--- config/routes.js
+++ config/routes.js
@@ -1,5 +1,8 @@
 const axios = require('axios');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
+const Users = require('./users-model.js');
 const { authenticate } = require('../auth/authenticate');
 
 module.exports = server => {
@@ -8,12 +11,54 @@ module.exports = server => {
   server.get('/api/jokes', authenticate, getJokes);
 };
 
+function generateToken(user) {
+  const payload = {
+    subject: user.id,
+    username: user.username,
+  };
+
+  const options = {
+    expiresIn: '2h'
+  };
+
+  return jwt.sign(payload, secrets.jwtSecret, options)
+}
+
 function register(req, res) {
   // implement user registration
+  let user = req.body;
+  const hash = bcrypt.hashSync(user.password, 10); // 2 ^ n
+  user.password = hash;
+
+  Users.add(user)
+    .then(saved => {
+      res.status(201).json(saved);
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
 }
 
 function login(req, res) {
   // implement user login
+  let { username, password } = req.body;
+
+  Users.findBy({ username })
+    .first()
+    .then(user => {
+      if (user && bcrypt.compareSync(password, user.password)) {
+        const token = generateToken(user);
+        res.status(200).json({
+          message: `Welcome ${user.username}!`,
+          token,
+        });
+      } else {
+        res.status(401).json({ message: "Invalid Credentials" });
+      }
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
 }
 
 function getJokes(req, res) {
@@ -30,3 +75,4 @@ function getJokes(req, res) {
       res.status(500).json({ message: 'Error Fetching Jokes', error: err });
     });
 }
+
